311e018d93ef96fd14787b7161e6893d5b3d3305
Martin Kouba <mkouba@redhat.com>
2021-07-01 14:39:17 +0200

    Loop - specify the timezone used in assertion

--- src/main/java/io/quarkus/qute/benchmark/Loop.java
+++ src/main/java/io/quarkus/qute/benchmark/Loop.java
@@ -1,11 +1,13 @@
 package io.quarkus.qute.benchmark;
 
 import java.math.BigDecimal;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.openjdk.jmh.annotations.Setup;
 
@@ -60,6 +62,7 @@ public class Loop extends BenchmarkBase {
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
+        cal.setTimeZone(TimeZone.getTimeZone(ZoneId.of("Europe/Prague")));
         item.setCreated(cal.getTime());
         item.setDescription(String.format("Item %s with price %s (created at %s)", item.getId(), item.getPrice(),
                 item.getCreated().getTime()));
