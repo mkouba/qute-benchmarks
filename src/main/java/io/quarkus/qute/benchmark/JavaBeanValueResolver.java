7c0b343547de0c45b3fd8415079ba791e6d483c5
Martin Kouba <mkouba@redhat.com>
2023-01-09 13:01:51 +0100

    JavaBeanValueResolver - set batchSize

--- src/main/java/io/quarkus/qute/benchmark/JavaBeanValueResolver.java
+++ src/main/java/io/quarkus/qute/benchmark/JavaBeanValueResolver.java
@@ -3,13 +3,17 @@ package io.quarkus.qute.benchmark;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
 
 import io.quarkus.qute.benchmark.data.JavaBean;
 
 /**
  * Test a generated value resolver generated for a pojo with 15 properties.
  */
+@Warmup(batchSize = 10)
+@Measurement(batchSize = 10)
 public class JavaBeanValueResolver extends BenchmarkBase {
 
     private final AtomicLong idGenerator = new AtomicLong();
