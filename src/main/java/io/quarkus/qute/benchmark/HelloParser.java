8ffaaed8293aa21b9b6bcbd36fb961ede027eb83
Martin Kouba <mkouba@redhat.com>
2023-09-13 15:30:02 +0200

    Re-add batchSize to HelloParser benchmark

--- src/main/java/io/quarkus/qute/benchmark/HelloParser.java
+++ src/main/java/io/quarkus/qute/benchmark/HelloParser.java
@@ -1,10 +1,14 @@
 package io.quarkus.qute.benchmark;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
 
 import io.quarkus.qute.Engine;
 
+@Warmup(batchSize = 1000)
+@Measurement(batchSize = 1000)
 public class HelloParser extends BenchmarkBase {
 
     private Engine engine;
