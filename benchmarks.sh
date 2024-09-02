be4e91793eef737f07894aa80dd249e2e10dcd52
Martin Kouba <mkouba@redhat.com>
2024-09-02 13:27:08 +0200

    Script - add an option to use gc/async profilers

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+contains_arg() {
+    local search="$1"
+    shift
+    local found=false
+
+    for arg in "$@"; do
+        if [[ "$arg" == "$search" ]]; then
+            found=true
+            break
+        fi
+    done
+
+    if [[ "$found" == true ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 # Min testable version is 2.7.0 
-QUTE_VERSIONS="2.16.12.Final 3.2.12.Final 3.8.6 3.14.1 999-SNAPSHOT";
+if [ -z $QUTE_VERSIONS ]; then
+    QUTE_VERSIONS="2.16.12.Final 3.2.12.Final 3.8.6 3.14.1 999-SNAPSHOT"
+fi
 
 # Set max to use Runtime.getRuntime().availableProcessors()
 THREADS="1"
@@ -13,14 +34,31 @@ else
     BENCHMARKS="HelloSimple|HelloParser|Loop15|Loop50|IfSimple|IfComplex|NameResolver|IncludeSimple|When|LetSimple|LetComplex|JavaBeanValueResolver"
 fi
 
+# Profilers
+ASYNC_PROFILER_PATH="/opt/java/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so"
+
+PROFILERS=""
+if contains_arg "-gc" "$@"; then
+    PROFILERS="$PROFILERS -prof gc"
+fi
+
+if contains_arg "-flame" "$@"; then
+    PROFILERS="$PROFILERS -prof async:libPath=$ASYNC_PROFILER_PATH;output=flamegraph;dir=profile-results"
+fi
+
+echo "============================================"
 echo "Qute versions to test: $QUTE_VERSIONS";
 echo "Benchmarks to run: $BENCHMARKS"
+echo "============================================"
+
+# Clean the target directory
+mvn clean
 
 QUTE_VERSIONS_ARRAY=$(echo $QUTE_VERSIONS);
 
 for i in $QUTE_VERSIONS_ARRAY
 do
   mvn package -Dversion.qute=$i
-  java -jar target/qute-benchmarks.jar -t $THREADS -rf json -rff target/results-$i.json $BENCHMARKS
+  java -jar target/qute-benchmarks.jar -t $THREADS $PROFILERS -rf json -rff target/results-$i.json $BENCHMARKS
   java -cp target/qute-benchmarks.jar io.quarkus.qute.benchmark.chart.ChartGenerator target
 done;
