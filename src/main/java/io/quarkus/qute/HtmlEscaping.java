1768e813174c02e262fd19f0d496edea3e12fd48
Martin Kouba <mkouba@redhat.com>
2025-01-28 13:37:40 +0100

    HtmlEscaping: remove the branchfull param

--- src/main/java/io/quarkus/qute/HtmlEscaping.java
+++ src/main/java/io/quarkus/qute/HtmlEscaping.java
@@ -25,122 +25,94 @@ import org.openjdk.jmh.annotations.Warmup;
 @Fork(2)
 public class HtmlEscaping {
 
-   private static final int SEED = 42;
-   @Param({ "100" })
-   private int latinCharsProbability;
-   @Param({ "0", "10" })
-   private int replacementProbability;
-   @Param({ "32" })
-   private int size;
-   @Param({ "100", "10000" })
-   private int samples;
-   @Param({ "true", "false" })
-   private boolean branchfull;
+    private static final int SEED = 42;
+    @Param({ "100" })
+    private int latinCharsProbability;
+    @Param({ "0", "10" })
+    private int replacementProbability;
+    @Param({ "32" })
+    private int size;
+    @Param({ "100", "10000" })
+    private int samples;
 
-   private String[] toEscape;
-   private Template escaper;
-   private int nextInput;
+    private String[] toEscape;
+    private Template escaper;
+    private int nextInput;
 
-   @Setup
-   public void prepareDataSet() {
-      final var specialChars = specialChars();
-      final var nonSpecialChars = nonSpecialChars(specialChars);
-      Random random = new Random(SEED);
-      toEscape = new String[samples];
-      var builder = new StringBuilder(size);
-      for (int i = 0; i < samples; i++) {
-         builder.setLength(0);
-         toEscape[i] = generateString(random, specialChars, nonSpecialChars, builder);
-      }
-      this.escaper = Engine.builder()
-            .addDefaults()
-            .addResultMapper(instantiateHtmlEscaper(branchfull))
-            .build()
-            .parse("{this}", Variant.forContentType(Variant.TEXT_HTML));
-      nextInput = 0;
-   }
+    @Setup
+    public void prepareDataSet() {
+        final var specialChars = specialChars();
+        final var nonSpecialChars = nonSpecialChars(specialChars);
+        Random random = new Random(SEED);
+        toEscape = new String[samples];
+        var builder = new StringBuilder(size);
+        for (int i = 0; i < samples; i++) {
+            builder.setLength(0);
+            toEscape[i] = generateString(random, specialChars, nonSpecialChars, builder);
+        }
+        this.escaper = Engine.builder()
+                .addDefaults()
+                .addResultMapper(new HtmlEscaper(List.of(Variant.TEXT_HTML)))
+                .build()
+                .parse("{this}", Variant.forContentType(Variant.TEXT_HTML));
+        nextInput = 0;
+    }
 
-   private String nextInput() {
-      String[] inputs = toEscape;
-      int index = nextInput;
-      index++;
-      if (index >= inputs.length) {
-         index = 0;
-      }
-      nextInput = index;
-      return inputs[index];
-   }
+    private String nextInput() {
+        String[] inputs = toEscape;
+        int index = nextInput;
+        index++;
+        if (index >= inputs.length) {
+            index = 0;
+        }
+        nextInput = index;
+        return inputs[index];
+    }
 
-   private String generateString(Random rnd, byte[] specialChars, int[] nonSpecialChars, StringBuilder builder) {
-      for (int i = 0; i < size; i++) {
-         int pickLatinChar = rnd.nextInt(100);
-         if (pickLatinChar < latinCharsProbability) {
-            int pickSpecialChar = rnd.nextInt(100);
-            if (pickSpecialChar < replacementProbability) {
-               byte ch = specialChars[rnd.nextInt(specialChars.length)];
-               builder.append((char) ch);
+    private String generateString(Random rnd, byte[] specialChars, int[] nonSpecialChars, StringBuilder builder) {
+        for (int i = 0; i < size; i++) {
+            int pickLatinChar = rnd.nextInt(100);
+            if (pickLatinChar < latinCharsProbability) {
+                int pickSpecialChar = rnd.nextInt(100);
+                if (pickSpecialChar < replacementProbability) {
+                    byte ch = specialChars[rnd.nextInt(specialChars.length)];
+                    builder.append((char) ch);
+                } else {
+                    char ch = (char) nonSpecialChars[rnd.nextInt(nonSpecialChars.length)];
+                    builder.append(ch);
+                }
             } else {
-               char ch = (char) nonSpecialChars[rnd.nextInt(nonSpecialChars.length)];
-               builder.append(ch);
+                builder.append((char) rnd.nextInt(256, Character.MAX_VALUE));
             }
-         } else {
-            builder.append((char) rnd.nextInt(256, Character.MAX_VALUE));
-         }
-      }
-      return builder.toString();
-   }
+        }
+        return builder.toString();
+    }
 
-   private static byte[] specialChars() {
-      return new byte[]{ '"', '\'', '&', '<', '>' };
-   }
+    private static byte[] specialChars() {
+        return new byte[] { '"', '\'', '&', '<', '>' };
+    }
 
-   private static int[] nonSpecialChars(byte[] specialChars) {
-      BitSet nonSpecialChars = new BitSet(256);
-      for (byte specialChar : specialChars) {
-         nonSpecialChars.set(specialChar);
-      }
-      nonSpecialChars.flip(0, 256);
-      int[] chars = new int[nonSpecialChars.cardinality()];
-      for (int i = nonSpecialChars.nextSetBit(0), j = 0; i >= 0; i = nonSpecialChars.nextSetBit(i + 1), j++) {
-         chars[j] = i;
-      }
-      return chars;
-   }
+    private static int[] nonSpecialChars(byte[] specialChars) {
+        BitSet nonSpecialChars = new BitSet(256);
+        for (byte specialChar : specialChars) {
+            nonSpecialChars.set(specialChar);
+        }
+        nonSpecialChars.flip(0, 256);
+        int[] chars = new int[nonSpecialChars.cardinality()];
+        for (int i = nonSpecialChars.nextSetBit(0), j = 0; i >= 0; i = nonSpecialChars.nextSetBit(i + 1), j++) {
+            chars[j] = i;
+        }
+        return chars;
+    }
 
-   @Benchmark
-   public void escape() {
-      escape(nextInput());
-   }
+    @Benchmark
+    public void escape() {
+        escape(nextInput());
+    }
 
-   @CompilerControl(CompilerControl.Mode.DONT_INLINE)
-   public String escape(String value) {
-      return escaper.render(value);
-   }
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public String escape(String value) {
+        return escaper.render(value);
+    }
 
-   private ResultMapper instantiateHtmlEscaper(boolean branchfull) {
-      if (branchfull) {
-         return new CharReplacementResultMapper() {
-
-            @Override
-            protected String replacementFor(char c) {
-               switch (c) {
-                  case '"':
-                     return "&quot;";
-                  case '\'':
-                     return "&#39;";
-                  case '&':
-                     return "&amp;";
-                  case '<':
-                     return "&lt;";
-                  case '>':
-                     return "&gt;";
-                  default:
-                     return null;
-               }
-            }
-         };
-      } else {
-         return new HtmlEscaper(List.of(Variant.TEXT_HTML));
-      }
-   }
 }
